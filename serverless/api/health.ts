85061b4601112d356dccc48ecad37714a51dff41
themillenniumfalcon <nishankpr2002@gmail.com>
2025-02-13 17:45:20 +0530

    work again?

--- serverless/api/health.ts
+++ serverless/api/health.ts
@@ -2,6 +2,6 @@ import { NextResponse } from 'next/server';
 
 export const runtime = 'edge';
 
-export default async function GET() {
+export async function GET(req: Request) {
   return NextResponse.json({ message: 'Hello World' });
 }
